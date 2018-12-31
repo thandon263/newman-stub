f3f65bd65abc3b4d25bf192181dc088feb213ecd
thandon263 <ncubethando20@gmail.com>
2018-12-31 14:59:12 -0500

    version 2.2.1: Working on files and return api response data.

--- newman/automation-runner.js
+++ newman/automation-runner.js
@@ -1,9 +1,10 @@
-const newman = require("newman")
-const { START, DONE } = require("./constants")
+const newman = require("newman");
+const fs = require("fs");
+const path = require("path");
+const { START, DONE } = require("./constants");
 
 module.exports = {
-
-    /**
+  /**
      * NMRunner accepts an argument of options and it will pass into the Newman runner.
      * The options are not optional, they are required in-order to run the test. 
      * Options should contain 
@@ -11,6 +12,7 @@ module.exports = {
      * @param {Object} options  
      * @return {void} `reports` 
      * @author dev36T
+     * @description Responses directory is created to store API responses.
      * @example
      *      options = {
      *          collection: "/path/to/collection.json",
@@ -19,21 +21,38 @@ module.exports = {
      *          ...
      *      }
      */
-    
-    runner: (options) => {
-        newman.run(options)
-        .on(START, function() {
-            process.stdout.write("The runner has started\n")
-        })
-        .on(DONE, function(err, summary) {
-            try {
-                // more work to do
-                return summary
-            } catch (err) {
-                return err
-            }
-        })
-    }
 
-    // TODO: Compare schema to response
-}
+  runner: options => {
+    newman
+      .run(options)
+      .on(START, function() {
+        process.stdout.write("The runner has started\n");
+      })
+      .on(DONE, function(err, summary) {
+        try {
+          // more work to do
+          fs.mkdirSync(path.join(__dirname, "../../responses"));
+          // Extract data stream from API
+          let summaryObj = summary.run.executions[0].response.stream.toString();
+        
+          const filepath = `../../responses/json-response-${new Date()}.json`;  
+          fs.writeFile(filepath, summaryObj, "utf-8", function(err, data) {
+            if (err) {
+              process.stdout.write("Failing to run!");
+              throw err;
+            }
+            process.stdout.write(
+              "Created Response file.",
+              "\nPath to Latest Response: ",
+              filepath
+            );
+            return data;
+          });
+        } catch (err) {
+          return err;
+        }
+      });
+  }
+
+  // TODO: Compare schema to response
+};
